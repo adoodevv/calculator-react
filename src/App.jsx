0513217af18a3ab1a3cd1456103ab597c1b153ad
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-20 02:43:49 +0000

    bootstrap edit for layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,17 @@ import Calculator from "./components/Calculator";
 
 const App = () => {
   return (
-    <div>
-      <h1>Calculator</h1>
-      <Calculator />
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="card-title text-center mb-4">Calculator</h1>
+              <Calculator />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
